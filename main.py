083d5a6d34e71ce496ca3668f00beb66fb08132d
lenabaraquin <baraquin.lena@gmail.com>
2024-11-01 23:58:47 +0100

    simplification du code et ajout d'une fonction framing

--- main.py
+++ main.py
@@ -4,93 +4,71 @@ import wave
 import struct
 
 
-def get_spectrum(waveform:list)->list:
-    spectrum = np.fft.rfft(waveform)
+def get_spectrum(waveform:list, samplerate:int)->list:
+    spectrum = np.fft.rfft(waveform, samplerate)
     return spectrum
 
-def get_log10_magnitude_spectrum(spectrum:list)->list:
-    log10_magnitude_spectrum = []
-    for i in spectrum:
-        i = abs(i)
-        i = np.log10(i)
-        log10_magnitude_spectrum.append(i)
-    return log10_magnitude_spectrum
+def get_cepstrum(spectrum:list)->list:
+    return np.fft.ifft(np.log10(np.abs(spectrum)))
 
-def get_power_spectrum(spectrum:list)->list:
-    power_spectrum = []
-    for i in spectrum:
-        i = abs(i)
-        i = i*i
-        power_spectrum.append(i)
-    return power_spectrum
-
-def get_cepstrum(power_spectrum:list)->list:
-    cepstrum = []
-    for i in power_spectrum:
-        i = np.log10(i)
-        cepstrum.append(i)
-    cepstrum = np.fft.ifft(cepstrum)
-    return cepstrum
-
-def lowpass_lifter(cepstrum:list, cutoff_quefrency:int=30)->list:
+def get_liftred_cepstrum(cepstrum:list, cutoff_quefrency:int=35)->list:
     for i in range(len(cepstrum)):
         if i>cutoff_quefrency:
             cepstrum[i]=0
     liftred_cepstrum = cepstrum
     return liftred_cepstrum
 
-def get_liftred_spectrum(liftred_cepstrum:list)->list:
-    smoothed_spectrum = np.abs(np.fft.rfft(liftred_cepstrum))
+def get_smoothed_spectrum(liftred_cepstrum:list)->list:
+    return np.abs(np.fft.rfft(liftred_cepstrum))
+
+def get_cepstrum_spectral_envelope(waveform:list, samplerate:int, lifter_cutoff_quefrency:int=35):
+    spectrum = get_spectrum(waveform, samplerate)
+    cepstrum = get_cepstrum(spectrum)
+    liftred_cepstrum = get_liftred_cepstrum(cepstrum, lifter_cutoff_quefrency)
+    smoothed_spectrum = get_smoothed_spectrum(liftred_cepstrum)
     return smoothed_spectrum
 
-def get_local_maxima(signal:list)->list:
-    local_maxima = []
-    for i in range(1, len(signal) - 1):
-        if signal[i] > signal[i-1] and signal[i] > signal[i+1]:
-            local_maxima.append(i)
-    return local_maxima
+def get_formants(spectral_envelope:list)->list:
+    formants = []
+    for i in range(1, len(spectral_envelope) - 1):
+        if spectral_envelope[i] > spectral_envelope[i-1] and spectral_envelope[i] > spectral_envelope[i+1]:
+            formants.append(i)
+    return formants
 
-def get_formants(smoothed_spectrum:list)->list:
-    return get_local_maxima(smoothed_spectrum)
+def framing(waveform:list, samplerate:int, frame_duration:float=0.1)->list:
+    frame_size = int(samplerate * frame_duration)
+    return [waveform[i:i + frame_size] for i in range(0, len(waveform), frame_size//2)]
 
-def get_waveform(sound)->list:
+def get_waveform(sound, sampwidth)->list:
     nframes = sound.getnframes()
     frames = sound.readframes(nframes)
-    #converts binary data to int values
-    samples = struct.unpack('h'*nframes, frames)
-    #    # Déterminer le format pour 'struct.unpack'
-    #    if sampwidth == 1:
-    #        fmt = 'B'  # unsigned char pour 8-bit (1 octet)
-    #    elif sampwidth == 2:
-    #        fmt = 'h'  # signed short pour 16-bit (2 octets)
-    #    elif sampwidth == 4:
-    #        fmt = 'i'  # signed int pour 32-bit (4 octets)
-    #    else:
-    #        raise ValueError(f"Unsupported sample width: {sampwidth} bytes")
-    #    fmt = '<' + fmt * nchannels  # Little-endian (<), répété pour chaque canal
-    #
-    #    # Convertir les frames en valeurs numériques
-    #    samples = struct.unpack(fmt * nframes, frames)
-    return samples
+    #converts binary data to numerical values
+    if sampwidth == 1:
+        samples = struct.unpack('B'*nframes, frames) # unsigned char pour 8-bit (1 octet)
+    elif sampwidth == 2:
+        samples = struct.unpack('h'*nframes, frames) # signed short pour 16-bit (2 octets)
+    elif sampwidth == 4:
+        samples = struct.unpack('i'*nframes, frames) # signed int pour 32-bit (4 octets)
+    else:
+        raise ValueError(f"Unsupported sample width: {sampwidth} bytes")
+    return list(samples)
 
 
 file_name = 'test_sounds/aaa.wav'
 with wave.open(file_name, 'rb') as sound:
     nchannels = sound.getnchannels()
     sampwidth = sound.getsampwidth()
-    framerate = sound.getframerate()
-    nframes = sound.getnframes()
+    samplerate = sound.getframerate()
+    nsamples = sound.getnframes()
 
-    waveform = get_waveform(sound)
-    spectrum = get_spectrum(waveform)
-    log10_magnitude_spectrum = get_log10_magnitude_spectrum(spectrum)
-    power_spectrum  = get_power_spectrum(spectrum)
-    cepstrum = get_cepstrum(power_spectrum)
-    liftred_cepstrum = lowpass_lifter(cepstrum)
-    smoothed_spectrum = get_liftred_spectrum(liftred_cepstrum)
-    formants = get_formants(smoothed_spectrum)
-    to_plot = smoothed_spectrum
+    waveform = get_waveform(sound, sampwidth)
+    frames = framing(waveform, samplerate, frame_duration=0.1)
+    for i in range(len(frames)):
+        spectral_envelope = get_cepstrum_spectral_envelope(frames[i], samplerate, 35)
+        formants = get_formants(spectral_envelope)
+        print(formants)
 
+    to_plot = spectral_envelope
     fig, ax = plt.subplots()
     ax.plot(to_plot)
     plt.show()

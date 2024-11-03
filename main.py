6a776b585005a8ae1e25f41fc0c24f10d27cb812
lenabaraquin <baraquin.lena@gmail.com>
2024-11-03 16:20:54 +0100

    encore une autre approche pour le tracking des formants

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import wave
 import struct
+import wave
+import numpy as np
 
-def get_waveform(file_path:str)->list:
-    with wave.open(file_name, 'rb') as sound:
-        nchannels = sound.getnchannels()
+
+def get_waveform(file_path:str)->tuple:
+    with wave.open(file_path, 'rb') as sound:
+        #nchannels = sound.getnchannels() if is stereo sound
         sampwidth = sound.getsampwidth()
         samplerate = sound.getframerate()
         nframes = sound.getnframes()
@@ -41,36 +42,57 @@ def framing(waveform:list, samplerate:int, frame_duration:float=0.1)->list:
     return [waveform[i:i + frame_size] for i in range(0, len(waveform), frame_size//2)]
 
 def hamming_windowing(frame:list)->list:
-    return frame*np.hamming(len(frame))
+    return list(frame*np.hamming(len(frame)))
 
-file_name = 'test_sounds/aaa.wav'
-(waveform, samplerate) = get_waveform(file_name)
+def pseudo_distance(x:tuple, y:tuple, r:float)->float:
+    x_1 = x[0]
+    x_2 = x[1]
+    y_1 = y[0]
+    y_2 = y[1]
+    d = np.sqrt(r*(x_1-y_1)**2 + (1/r)*(x_2-y_2)**2)
+    return d
+
+def get_nearest_point(point:tuple, points_to_compare:list)->tuple:
+    r = 0.1
+    dist_of_nearest_point = (pseudo_distance(point, points_to_compare[0], r),(0, 0))
+    for x in points_to_compare:
+        d = pseudo_distance(point, x, r)
+        if d < dist_of_nearest_point[0]:
+            dist_of_nearest_point = (d, x)
+    return dist_of_nearest_point[1]
+
+def plot_ball():
+    to_plot=([], [])
+    for i in np.arange(-2, 2, 0.01):
+        for j in np.arange(-2, 2, 0.01):
+            if 0.9 < pseudo_distance((0,0), (i,j), 0.1) < 1.1:
+                to_plot[0].append(i)
+                to_plot[1].append(j)
+    return to_plot
+
+file_path = 'test_sounds/aaa.wav'
+(waveform, samplerate) = get_waveform(file_path)
 frames = framing(waveform, samplerate, frame_duration=0.1)
 list_of_formants = []
 for i in range(len(frames)):
     frame = hamming_windowing(frames[i])
     spectral_envelope = get_cepstrum_spectral_envelope(frame, samplerate, 30)
-    formants = get_formants(spectral_envelope)
+    formants = get_formants(list(spectral_envelope))
     list_of_formants.append(formants)
 
 formants_in_time = [] #vectors in the time-frequency space
 for i in range(len(list_of_formants)):
     for j in range(len(list_of_formants[i])):
-        formants_in_time.append((i,list_of_formants[i][j]))
+        formants_in_time.append((i, list_of_formants[i][j], flag:=True))
 print(formants_in_time)
 
-for i in range(len(formants_in_time)):
-    (time, formant)=formants_in_time[i]
-    #formant_set = {(time, formant)}
-    #def set de tous les formants des couples de forme (time+1, formant)
-    #def set formant-r, formant+r
-    #set1.intersection(set2)
-    #new_formant = si taille 1, continuer, si taille plus de 1 prendre le plus proche de formant, si inter vide, stop et passe à formants_in_time[i+1]
-    #formant_set.append(new_formant)
-    #recommancer avec (time+1, formant) jusqu'a ce que l'intersection soit vide
-    #envoyer formant_set dans une liste à return 
+#r = 0.1
+for (time, formant, flag) formants_in_time:
+    if flag:
+        formant_set = {(time, formant)}
+        #aller voir le point le plus proche parmi les flag true 
+        # si inferieur à seuil, alors ajouter dans le set et passer le flag en false, sinon exit, ajouter formant_set à ce qui sera return et i suivant (avec une while ?)
 
-#    to_plot = spectral_envelope
-#    fig, ax = plt.subplots()
-#    ax.plot(to_plot)
-#    plt.show()
+#to_plot=plot_ball()
+#plt.plot(to_plot[0], to_plot[1], 'o')
+#plt.show()

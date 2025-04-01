75c4751f43e56c95299c8e0e7dc5aa32a94c42df
Léna Baraquin <baraquin.lena@gmail.com>
2025-04-01 11:43:01 +0200

    amelioration de la fonction de recherche de maxima locaux

--- main.py
+++ main.py
@@ -4,78 +4,90 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+
 class Formants:
-    def __init__(self, file_path:str, frame_duration:float=0.1, cutoff_quefrency:int=35) -> None:
+    def __init__(
+        self, file_path: str, frame_duration: float = 0.1, cutoff_quefrency: int = 35
+    ) -> None:
         self.file_path = file_path
         self.frame_duration = frame_duration
         self.cutoff_quefrency = cutoff_quefrency
         self._extract_waveform()
         self._extract_spectrogram()
 
-
-    def _extract_waveform(self)-> None:
+    def _extract_waveform(self) -> None:
         """from a file path extract samplerate and list of samples"""
-        with wave.open(self.file_path, 'rb') as sound:
-            #nchannels = sound.getnchannels() if is stereo sound
+        with wave.open(self.file_path, "rb") as sound:
+            # nchannels = sound.getnchannels() if is stereo sound
             sampwidth = sound.getsampwidth()
             samplerate = sound.getframerate()
             nframes = sound.getnframes()
             frames = sound.readframes(nframes)
-        #converts binary data to numerical values
+        # converts binary data to numerical values
         if sampwidth == 1:
-            samples = struct.unpack('B'*nframes, frames) # unsigned char for 8-bit
+            samples = struct.unpack("B" * nframes, frames)  # unsigned char for 8-bit
         elif sampwidth == 2:
-            samples = struct.unpack('h'*nframes, frames) # signed short for 16-bit
+            samples = struct.unpack("h" * nframes, frames)  # signed short for 16-bit
         elif sampwidth == 4:
-            samples = struct.unpack('i'*nframes, frames) # signed int for 32-bit
+            samples = struct.unpack("i" * nframes, frames)  # signed int for 32-bit
         else:
             raise ValueError(f"Unsupported sample width: {sampwidth} bytes")
         self.samplerate = samplerate
         self.sound_waveform = list(samples)
-        self.sound_duration = len(samples)/samplerate
+        self.sound_duration = len(samples) / samplerate
 
     def _extract_spectral_envelope(self, analyzed_waveform):
         """from a waveform give his spectral envelope with the cepstrum method"""
         spectrum = np.fft.rfft(analyzed_waveform, self.samplerate)
         log10_magnitude_spectrum = np.log10(np.abs(spectrum))
         cepstrum = np.fft.irfft(log10_magnitude_spectrum)
-        liftred_cepstrum = [0 if i > self.cutoff_quefrency else x for i, x in enumerate(cepstrum)]
+        liftred_cepstrum = [
+            0 if i > self.cutoff_quefrency else x for i, x in enumerate(cepstrum)
+        ]
         smoothed_spectrum = np.abs(np.fft.rfft(liftred_cepstrum))
         return list(smoothed_spectrum)
 
-    def _framing(self)->list:
+    def _framing(self) -> list:
         """split the waveform in several frames"""
         frame_size = int(self.samplerate * self.frame_duration)
-        self.middle_frame = self.sound_waveform[len(self.sound_waveform)//2:len(self.sound_waveform)//2+frame_size]
-        return [self.sound_waveform[i:i + frame_size] for i in range(0, len(self.sound_waveform), frame_size)]
+        self.middle_frame = self.sound_waveform[
+            len(self.sound_waveform) // 2 : len(self.sound_waveform) // 2 + frame_size
+        ]
+        return [
+            self.sound_waveform[i : i + frame_size]
+            for i in range(0, len(self.sound_waveform), frame_size)
+        ]
 
     @staticmethod
-    def _hamming_windowing(frame:list)->list:
+    def _hamming_windowing(frame: list) -> list:
         """windowing with the hamming function"""
-        return list(frame*np.hamming(len(frame)))
+        return list(frame * np.hamming(len(frame)))
 
     @staticmethod
-    def _extract_formants(spectral_envelope:list)->list:
-        """compute formants from the spectral envelope of a waveform""" #ameliorer la fonction de recherche de maxima locaux pour qu'elle soit robuste en cas de plateaux
+    def _extract_formants(spectral_envelope: list) -> list:
+        """compute formants from the spectral envelope of a waveform"""
         formants = []
         for i in range(1, len(spectral_envelope) - 1):
-            if spectral_envelope[i] > spectral_envelope[i-1] and spectral_envelope[i] > spectral_envelope[i+1]: 
+            if (
+                spectral_envelope[i] > spectral_envelope[i - 1]
+                and spectral_envelope[i] >= spectral_envelope[i + 1]
+            ):
                 formants.append(i)
         return formants
 
-    def _extract_spectrogram(self)->None:
+    def _extract_spectrogram(self) -> None:
         """return a list of (time, frequency, intensity) tuples, the time window length depend on self.frame_duration"""
         spectrogram = []
         matrix_spectrogram = []
         splited_waveform = self._framing()
         ham_splited_waveform = []
-        formants_list = ([], []) #frequency value, time
+        formants_list = ([], [])  # frequency value, time
         for frame in splited_waveform:
             ham_splited_waveform.append(self._hamming_windowing(frame))
         for i in range(len(ham_splited_waveform)):
-            time_slice = [] #list to add to matrix_spectrogram
+            time_slice = []  # list to add to matrix_spectrogram
             frame = ham_splited_waveform[i]
-            time = i*self.frame_duration
+            time = i * self.frame_duration
             spectral_envelope = self._extract_spectral_envelope(frame)
             formant_list = self._extract_formants(spectral_envelope)
             for j in range(len(formant_list)):
@@ -91,31 +103,32 @@ class Formants:
         self.matrix_spectrogram = matrix_spectrogram
         self.formants = formants_list
 
+
 aaa_file_path = "test_sounds/aaa.wav"
 iii_file_path = "test_sounds/iii.wav"
 uuu_file_path = "test_sounds/uuu.wav"
 
-#with tuned cutoff_quefrency
+# with tuned cutoff_quefrency
 aaa = Formants(aaa_file_path, 0.01, 31)
-iii = Formants(iii_file_path, 0.2, 38)
-uuu = Formants(uuu_file_path, 0.2, 49)
+iii = Formants(iii_file_path, 0.02, 38)
+uuu = Formants(uuu_file_path, 0.02, 49)
 
-#expected firsts formants for test 
+# expected firsts formants for test
 aaa_expected_formants = [900, 1400, 2650]
 iii_expected_formants = [255, 2100, 3650]
 uuu_expected_formants = [350, 570, 2600]
 
 ## Dimensions de l'image
-#width, height = 1000, 4000
+# width, height = 1000, 4000
 #
 ## Créer une image en nuances de gris
-#image = Image.new('L', (width, height))
+# image = Image.new('L', (width, height))
 #
 ## Accéder à l'objet PixelAccess
-#pixels = image.load()
+# pixels = image.load()
 #
 ## Modifier les pixels pour créer un dégradé horizontal
-#for x in range(len(aaa.matrix_spectrogram)):
+# for x in range(len(aaa.matrix_spectrogram)):
 #    for y in range(len(aaa.matrix_spectrogram[x])):
 #        # Valeur du gris (ici basée sur la position x)
 #        if x < width/10 and y < height:
@@ -125,7 +138,7 @@ uuu_expected_formants = [350, 570, 2600]
 #                print(gray_value)
 #
 ## Sauvegarder ou afficher l'image
-#image.show()  # Pour afficher
+# image.show()  # Pour afficher
 
-plt.plot(aaa.formants[0], aaa.formants[1], 'o')
+plt.plot(iii.formants[0], iii.formants[1], "o")
 plt.show()
